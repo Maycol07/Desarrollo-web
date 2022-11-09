8764af4688b3123126efdee59c2e24791cce104d
Mrx18 <115897516+MaycolRichardClaudioPinedo18@users.noreply.github.com>
2022-11-09 16:16:40 -0500

    css

--- resources/views/Inicio/index.blade.php
+++ resources/views/Inicio/index.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Remember Bootstrap Template - Index</title>
+  <title>LLATA</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
