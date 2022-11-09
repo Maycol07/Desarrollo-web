0d496174cd34c5a3a9e76c945b09e1a2a40bfe21
Mrx18 <115897516+MaycolRichardClaudioPinedo18@users.noreply.github.com>
2022-11-09 18:14:46 -0500

    Portada

--- resources/views/Inicio/index.blade.php
+++ resources/views/Inicio/index.blade.php
@@ -58,36 +58,36 @@
     <div class="container d-flex justify-content-between">
 
       <div class="logo">
-        <h1 class="text-light"><a href="index.html">Remember</a></h1>
+        <h1 class="text-light"><a href="index.html">LLATA</a></h1>
         <!-- Uncomment below if you prefer to use an image logo -->
         <!-- <a href="index.html"><img src="{{ asset('home/assets/img/logo.png')}}" alt="" class="img-fluid"></a>-->
       </div>
 
       <nav id="navbar" class="navbar">
         <ul>
-          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-          <li><a class="nav-link scrollto" href="#about">About</a></li>
-          <li><a class="nav-link scrollto" href="#services">Services</a></li>
-          <li><a class="nav-link scrollto " href="#portfolio">Portfolio</a></li>
-          <li><a class="nav-link scrollto" href="#team">Team</a></li>
-          <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
+          <li><a class="nav-link scrollto active" href="#hero">ARQUEOLOGIA</a></li>
+          <li><a class="nav-link scrollto" href="#about">DANZA</a></li>
+          <li><a class="nav-link scrollto" href="#services"></a></li>
+          <li><a class="nav-link scrollto " href="#portfolio">GASTRONOMIA</a></li>
+          <li><a class="nav-link scrollto" href="#team">MUSICA</a></li>
+          <li class="dropdown"><a href="#"><span>GEOGRAFIA</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
-              <li><a href="#">Drop Down 1</a></li>
-              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+              <li><a href="#">LLATA</a></li>
+              <li class="dropdown"><a href="#"><span>COCHAPATA</span> <i class="bi bi-chevron-right"></i></a>
                 <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
+                  <li><a href="#">CHAVIN</a></li>
+                  <li><a href="#">PUÑOS</a></li>
+                  <li><a href="#">TANTAMAYO</a></li>
+                  <li><a href="#">SINGA</a></li>
+                  <li><a href="#">MONZON</a></li>
                 </ul>
               </li>
-              <li><a href="#">Drop Down 2</a></li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
+              <li><a href="#">JIRCAN</a></li>
+              <li><a href="#">JACAS GRANDE</a></li>
+              <li><a href="#">ARANCAY</a></li>
             </ul>
           </li>
-          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+          <li><a class="nav-link scrollto" href="#contact">CONTACTO</a></li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
@@ -98,9 +98,9 @@
   <!-- ======= Hero Section ======= -->
   <section id="hero">
     <div class="hero-container" data-aos="fade-up">
-      <h1>Welcome to Remember</h1>
-      <h2>We are team of talented designers making websites with Bootstrap</h2>
-      <a href="#about" class="btn-get-started scrollto">Get Started</a>
+      <h1>Visite Llata</h1>
+      <h2>Una tierra noble y hospitalaria</h2>
+      <a href="#about" class="btn-get-started scrollto">Te esperamos!</a>
     </div>
   </section><!-- End Hero -->
 
