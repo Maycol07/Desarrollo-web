628f48f48cac3b52acb939d3a3f587be8424fb7a
Mrx18 <115897516+MaycolRichardClaudioPinedo18@users.noreply.github.com>
2022-11-10 08:35:45 -0500

    api terminado

--- resources/views/Inicio/index.blade.php
+++ resources/views/Inicio/index.blade.php
@@ -636,20 +636,20 @@ El Distrito peruano de Llata es uno de los once distritos de la Provincia de Hua
             <div class="info">
               <div class="address">
                 <i class="bi bi-geo-alt"></i>
-                <h4>Location:</h4>
-                <p>A108 Adam Street, New York, NY 535022</p>
+                <h4>Localizacion:</h4>
+                <p>Llata, Provincia de Huamalíes, Huanuco</p>
               </div>
 
               <div class="email">
                 <i class="bi bi-envelope"></i>
                 <h4>Email:</h4>
-                <p>info@example.com</p>
+                <p>Maycolclaudiopinedo@gmail.com</p>
               </div>
 
               <div class="phone">
                 <i class="bi bi-phone"></i>
-                <h4>Call:</h4>
-                <p>+1 5589 55488 55s</p>
+                <h4>Celular:</h4>
+                <p>+51 916183260</p>
               </div>
 
             </div>
